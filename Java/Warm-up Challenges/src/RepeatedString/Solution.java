866d2fa3b72dbbf22a80e64e4a1b032fe117940d
Rusev11 <ivaylo.rusev@outlook.com>
2021-03-26 11:50:24 +0200

    Repeated String Challenge Updated

--- Java/Warm-up Challenges/src/RepeatedString/Solution.java
+++ Java/Warm-up Challenges/src/RepeatedString/Solution.java
@@ -1,52 +1,30 @@
 package RepeatedString;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
 public class Solution {
 
     // Complete the repeatedString function below.
     static long repeatedString(String s, long n) {
-        long number = 0;
-        String newS = "";
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            newS += s.charAt(j);
-            j++;
-            if (j == s.length()) {
-                j = 0;
+        long numberInString = 0;
+        long numberInNStrings = 0;
+        long numberInRestString = 0;
+        long restStringLength = n % s.length();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                numberInString += 1;
             }
         }
-        for (int i = 0; i < n; i++) {
-            if(newS.charAt(i)=='a'){
-                number++;
+        long stringRepeat = n / s.length();
+        numberInNStrings = numberInString * stringRepeat;
+        for (int i = 0; i < restStringLength; i++) {
+            if (s.charAt(i) == 'a') {
+                numberInRestString += 1;
             }
         }
-        return number;
+
+        return numberInNStrings + numberInRestString;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String s = scanner.nextLine();
-
-        long n = scanner.nextLong();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        long result = repeatedString(s, n);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+    public static void main(String[] args) {
+        System.out.println(repeatedString("bcbccacaacbbacabcabccacbccbababbbbabcccbbcbcaccababccbcbcaabbbaabbcaabbbbbbabcbcbbcaccbccaabacbbacbc", 649606239668L));
     }
 }
