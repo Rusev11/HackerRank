421fb3f198f0bb5b1ff7dae57370dcc9bcdc77ce
Rusev11 <ivaylo.rusev@outlook.com>
2021-04-02 10:07:57 +0300

    Delete node at specific position in linked list challenge completed

---
+++ Java/Data Structures/src/DeleteNodeInLinkedList.java
@@ -0,0 +1,98 @@
+import java.io.IOException;
+
+
+public class DeleteNodeInLinkedList {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node) throws IOException {
+        while (node != null) {
+            System.out.println(node.data);
+            node = node.next;
+        }
+    }
+
+
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        if (llist == null) {
+            llist = new SinglyLinkedListNode(data);
+            return llist;
+        }
+        SinglyLinkedListNode newHead = new SinglyLinkedListNode(data);
+        newHead.next = llist;
+        return newHead;
+    }
+
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        if (position == 0) {
+            SinglyLinkedListNode newHead = new SinglyLinkedListNode(data);
+            newHead.next = head;
+            return newHead;
+        }
+        SinglyLinkedListNode tempNode = head;
+        for (int i = 1; i < position; i++) {
+            tempNode = tempNode.next;
+        }
+        SinglyLinkedListNode newHead = new SinglyLinkedListNode(data);
+        newHead.next = tempNode.next;
+        tempNode.next = newHead;
+        return head;
+    }
+
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+        if(head == null){
+            return head;
+        }
+        if(position == 0){
+            return head.next;
+        }
+
+        SinglyLinkedListNode tempNode = head;
+        for (int i = 1; i < position; i++) {
+            tempNode = tempNode.next;
+        }
+        if(tempNode.next.next ==null){
+            tempNode.next = null;
+            return head;
+        }
+        tempNode.next = tempNode.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) throws IOException {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int[] input = {7, 13, 16};
+
+        for (int i = 0; i < input.length; i++) {
+
+            int llistItem = input[i];
+
+            SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+
+            llist.head = llist_head;
+        }
+
+//        insertNodeAtPosition(llist.head, 1, 2);
+
+        printSinglyLinkedList(llist.head);
+    }
+}
